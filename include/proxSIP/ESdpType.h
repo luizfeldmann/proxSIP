66122de5e25f5fbd0e585998e5381f618cd4f872
Luiz Gustavo Pfitscher e Feldmann <lfeldmann@ael.com.br>
2023-05-20 14:59:10 -0300

    Support for SDP network type and address type enumerations

--- include/proxSIP/ESdpType.h
+++ include/proxSIP/ESdpType.h
@@ -1,6 +1,9 @@
 #ifndef _ESDPTYPE_H_
 #define _ESDPTYPE_H_
 
+#include "proxSIPExports.h"
+
+//! Each single-character code defining data fields in SDP lines
 enum class ESdpType : char
 {
     Version     = 'v',
@@ -19,5 +22,37 @@ enum class ESdpType : char
     Repeat      = 'r',
     Media       = 'm',
 };
+
+//! Types of network
+enum class ESdpNetType
+{
+    Unknown = 0,
+    IN, //!< Internet
+};
+
+extern PROXSIP_API const char* c_szSdpIN; //!< Text for #ESdpNetType::IN
+
+//! Converts the text network type to the enum
+PROXSIP_API ESdpNetType SdpGetNetTypeEnum(const char*);
+
+//! Converts the enum network type to a string
+PROXSIP_API const char* SdpGetNetTypeStr(ESdpNetType);
+
+//! Types of address
+enum class ESdpAddrType
+{
+    Unknown,
+    IP4 = 4,    //!< Internet Protocol v. 4
+    IP6 = 6,    //!< Internet Protocol v. 6
+};
+
+extern PROXSIP_API const char* c_szSdpIP4; //!< Text for #ESdpAddrType::IP4
+extern PROXSIP_API const char* c_szSdpIP6; //!< Text for #ESdpAddrType::IP6
+
+//! Converts the text address type to the enum
+PROXSIP_API ESdpAddrType SdpGetAddrTypeEnum(const char*);
+
+//! Converts the enum address type to a string
+PROXSIP_API const char* SdpGetAddrTypeStr(ESdpAddrType);
 
 #endif
---
+++ src/proxSIP/ESdpType.cpp
@@ -0,0 +1,68 @@
+#include "ESdpType.h"
+#include <cstring>
+
+const char* c_szSdpIN = "IN";
+const char* c_szSdpIP4 = "IP4";
+const char* c_szSdpIP6 = "IP6";
+
+ESdpNetType SdpGetNetTypeEnum(const char* sText)
+{
+    ESdpNetType eVal = ESdpNetType::Unknown;
+
+    if (0 == strcmp(sText, c_szSdpIN))
+        eVal = ESdpNetType::IN;
+
+    return eVal;
+}
+
+const char* SdpGetNetTypeStr(ESdpNetType eVal)
+{
+    const char* sText = nullptr;
+
+    switch (eVal)
+    {
+    default:
+    case ESdpNetType::Unknown:
+        break;
+
+    case ESdpNetType::IN:
+        sText = c_szSdpIN;
+        break;
+    }
+
+    return sText;
+}
+
+ESdpAddrType SdpGetAddrTypeEnum(const char* sText)
+{
+    ESdpAddrType eVal = ESdpAddrType::Unknown;
+
+    if (0 == strcmp(sText, c_szSdpIP4))
+        eVal = ESdpAddrType::IP4;
+    else if (0 == strcmp(sText, c_szSdpIP6))
+        eVal = ESdpAddrType::IP6;
+
+    return eVal;
+}
+
+const char* SdpGetAddrTypeStr(ESdpAddrType eVal)
+{
+    const char* sText = nullptr;
+
+    switch (eVal)
+    {
+    default:
+    case ESdpAddrType::Unknown:
+        break;
+
+    case ESdpAddrType::IP4:
+        sText = c_szSdpIP4;
+        break;
+
+    case ESdpAddrType::IP6:
+        sText = c_szSdpIP6;
+        break;
+    }
+
+    return sText;
+}
